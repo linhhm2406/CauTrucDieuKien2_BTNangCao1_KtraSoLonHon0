441c533a8b2eb7ce730d86b40e21a65fc66c9fdd
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:07:32 +0700

    Add file

---
+++ my.js
@@ -0,0 +1,8 @@
+let number=parseInt(prompt('Nhap vao so nguyen can kiem tra'));
+if (number>0) {
+    alert('So lon hon 0');
+}else if(number<0){
+    alert('So nho hon 0')
+}else {
+    alert('So bang 0');
+}
